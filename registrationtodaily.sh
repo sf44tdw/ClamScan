27f5781ebc91bfb152590fd1efd346e516103372
sf44tdw <prmkaddr@yahoo.co.jp>
2017-04-23 09:16:45 +0900

    作成

---
+++ registrationtodaily.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+THIS_DIR=$(cd $(dirname $0);pwd)
+SCRIPT="clamscan"
+SRC=${THIS_DIR}/${SCRIPT}
+
+TARGET_DIR="/etc/cron.daily"
+DEST=${TARGET_DIR}/${SCRIPT}
+
+#コピー先に既にある場合は上書きする。
+cp -f ${SRC} ${TARGET_DIR}
+
+#root:rootかつ644に変更。
+chown root:root ${DEST}
+chmod 755 ${DEST}
+
+ls -l ${TARGET_DIR}
---
+++ removefromdailly.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+SCRIPT="clamscan"
+TARGET_DIR="/etc/cron.daily"
+DEST="${TARGET_DIR}/${SCRIPT}"
+
+rm -f ${DEST}
+
+ls -l ${TARGET_DIR}
