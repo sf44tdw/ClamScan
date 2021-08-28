9f5b4a6895b6068de940098b9ed608dbbe869a49
root <root@localhost.localdomain>
2021-08-28 16:37:38 +0900

    clamav

--- registrationtodaily.sh
+++ registrationtodaily.sh
@@ -7,11 +7,11 @@ SRC=${THIS_DIR}/${SCRIPT}
 TARGET_DIR="/etc/cron.daily"
 DEST=${TARGET_DIR}/${SCRIPT}
 
+#root:rootかつ644に変更。
+chown root:root ${SRC}
+chmod 755 ${SRC}
+
 #コピー先に既にある場合は上書きする。
 cp -f ${SRC} ${TARGET_DIR}
-
-#root:rootかつ644に変更。
-chown root:root ${DEST}
-chmod 755 ${DEST}
 
 ls -l ${TARGET_DIR}
