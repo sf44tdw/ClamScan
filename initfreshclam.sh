4921afabb337152a4ecc75cccbece37f9edc66d7
sf44tdw <sf44tdw@users.noreply.github.com>
2022-07-16 03:26:22 +0900

    不足分

---
+++ initfreshclam.sh
@@ -0,0 +1,6 @@
+FRESHCLAM_CONFIG_FILE_NAME="/etc/freshclam.conf"
+
+cp -a ${FRESHCLAM_CONFIG_FILE_NAME}{,.`date +%Y%m%d%H%M%S`.bak} || exit 1
+sed -i '/^Example/s/^/#/' ${FRESHCLAM_CONFIG_FILE_NAME}
+sed -i 's/^#NotifyClamd \/path\/to\/clamd.conf/NotifyClamd \/etc\/clamd.d\/scan.conf/g' ${FRESHCLAM_CONFIG_FILE_NAME}
+freshclam
